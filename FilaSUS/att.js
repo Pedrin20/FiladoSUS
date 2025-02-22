c9dd7413563d482bf9008e5d05f0f9e1aac28569
Pedrin20 <145607042+Pedrin20@users.noreply.github.com>
2025-02-22 18:14:38 -0300

    Atualizando

--- FilaSUS/att.js
+++ FilaSUS/att.js
@@ -1,78 +1,35 @@
+let fila = [];
+let opcao = " ";
 
-class FilaEspera {
-  constructor() {
-    this.fila = [];
-  }
-
-  filaPreferencial(paciente) {
-    this.fila.unshift(paciente);
-    console.log(`${paciente} adicionado a fila preferencial!`);
-  }
-
-
-
-  adicionarPaciente(paciente) {
-    const now = new Date();
-    const horas = now.getHours().toString().padStart(2, '0');
-    const minutos = now.getMinutes().toString().padStart(2, '0');
-    const horaFormatada = `${horas}:${minutos}`;
-    
-    this.fila.push(paciente);
-    console.log(`${paciente} adicionado à fila, as ${horaFormatada}`);
-  }
-
-  proximoPaciente() {
-    if (this.fila.length === 0) {
-      console.log("Não há pacientes na fila.");
-    } else {
-      const proximo = this.fila.shift();
-      console.log(`Próximo paciente: ${proximo}`);
-    }
-  }
-
-  consultarFila() {
-    console.log("Fila de espera:");
-    this.fila.forEach((paciente, index) => {
-      console.log(`${index + 1}. ${paciente}`);
-    });
-  }
-}
-
-
-// Criando uma instância da fila
-const filaConsultorio = new FilaEspera();
-
-// Menu de interação
-let opcao;
 do {
-  console.log("\n--- Fila de Espera ---");
-  console.log("1. Adicionar paciente");
-  console.log("2. Próximo paciente");
-  console.log("3. Adicionando na fila preferencial");
-  console.log("4. Consultar fila");
-  console.log("0. Sair");
-  
-  opcao = parseInt(prompt("Digite a opção desejada: "));
+  let pacientes = "";
+  for (let i = 0; i < fila.length; i++) {
+    pacientes += (i + 1) + " - " + fila[i] + "\n";
+  }
+
+  opcao = prompt(
+    "Pacientes: \n" + pacientes +
+    "\nEscolha uma opção: \n1. Novo Paciente \n2. Atender Paciente \n3. Sair"
+  );
 
   switch (opcao) {
-    case 1:
-      const novoPaciente = prompt("Digite o nome do paciente: ");
-      filaConsultorio.adicionarPaciente(novoPaciente);
+    case "1":
+      let nome = prompt("Digite o nome do paciente: ");
+      fila.push(nome);
       break;
-    case 2:
-      filaConsultorio.proximoPaciente();
+    case "2":
+      if (fila.length > 0) {
+        alert("Atendendo paciente: " + fila.shift());
+      } else {
+        alert("Fila vazia!");
+      }
       break;
-    case 3:
-    const preferencialPaciente = prompt("Digite o nome do idoso: ")  
-    filaConsultorio.filaPreferencial(preferencialPaciente +" (preferencial)");
-      break;
-    case 4:
-     filaConsultorio.consultarFila();
-     break;
-    case 0:
-      console.log("Saindo...");
+    case "3":
+      alert("Saindo...");
       break;
     default:
-      console.log("Opção inválida.");
+      alert("Opção inválida!");
+      break;
   }
-} while (opcao !== 0);
+} while (opcao !== "3");
+
